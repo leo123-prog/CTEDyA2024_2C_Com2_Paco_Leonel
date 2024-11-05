050c05a39d29f99f6423a155696b843f0768ebe0
Leonel Paco <lpaco249@gmail.com>
2024-11-04 21:01:03 -0300

    Add files via upload

---
+++ Entreaga_n°1_Corregido/ejercicio1/ArbolBinario.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio1
+{
+
+    public class ArbolBinario<T>
+    {
+        
+        private T dato;
+        private ArbolBinario<T> hijoIzquierdo;
+        private ArbolBinario<T> hijoDerecho;
+
+
+        public ArbolBinario(T dato)
+        {
+            this.dato = dato;
+
+        }
+
+        public void setear_raiz(T dato) //set al dato del nodo. 
+        {
+            this.dato = dato;
+        }
+
+       
+         //retornamos el dato de la raiz.
+        public T getDatoRaiz()
+        {
+            return this.dato;
+        }
+
+        public ArbolBinario<T> getHijoIzquierdo()
+        {
+            return this.hijoIzquierdo;
+        }
+
+        public ArbolBinario<T> getHijoDerecho()
+        {
+            return this.hijoDerecho;
+        }
+
+
+        public void agregarHijoIzquierdo(ArbolBinario<T> hijo)
+        {
+            this.hijoIzquierdo = hijo;
+        }
+
+        public void agregarHijoDerecho(ArbolBinario<T> hijo)
+        {
+            this.hijoDerecho = hijo;
+        }
+
+
+        public void eliminarHijoIzquierdo()
+        {
+            this.hijoIzquierdo = null;
+        }
+
+        public void eliminarHijoDerecho()
+        {
+            this.hijoDerecho = null;
+        }
+
+        public bool esHoja()
+        {
+            return this.hijoIzquierdo == null && this.hijoDerecho == null;
+        }
+        public void inorden()
+        {
+            if (this.hijoIzquierdo != null)
+                this.hijoIzquierdo.inorden();
+
+            Console.Write(this.dato + " ");
+
+            if (this.hijoDerecho != null)
+                this.hijoDerecho.inorden();
+
+        }
+
+        public void preorden()
+        {
+            Console.Write(this.dato + " ");
+
+            if (this.hijoIzquierdo != null)
+                this.hijoIzquierdo.preorden();
+
+
+            if (this.hijoDerecho != null)
+                this.hijoDerecho.preorden();
+
+        }
+
+        public void postorden()
+        {
+
+            if (this.hijoIzquierdo != null) 
+            this.hijoIzquierdo.postorden();
+
+            if (this.hijoDerecho != null) 
+            this.hijoDerecho.postorden();
+
+            Console.Write(this.dato + " ");
+        }
+
+        public void recorridoPorNiveles()
+        {
+            Cola<ArbolBinario<T>> cola = new Cola<ArbolBinario<T>>();
+            cola.encolar(this); // Empezamos con la raíz
+
+            while (cola.cuantos() > 0)
+            {
+                ArbolBinario<T> actual = cola.desencolar();
+                Console.Write(actual.getDatoRaiz()+" "); // Procesamos el nodo actual
+
+                // Agregamos el hijo izquierdo si existe
+                if (actual.getHijoIzquierdo() != null)
+                {
+                    cola.encolar(actual.getHijoIzquierdo());
+                }
+
+                // Agregamos el hijo derecho si existe
+                if (actual.getHijoDerecho() != null)
+                {
+                    cola.encolar(actual.getHijoDerecho());
+                }
+
+
+            }
+        }
+        public int contarHojas()
+        {
+            if (this.esHoja())
+            {
+                return 1; // Si el nodo es hoja, devolvemos 1
+            }
+
+            int contador = 0;
+
+            // Llamada recursiva al hijo izquierdo si existe
+            if (this.getHijoIzquierdo() != null)
+            {
+                contador += this.getHijoIzquierdo().contarHojas();
+            }
+
+            // Llamada recursiva al hijo derecho si existe
+            if (this.getHijoDerecho() != null)
+            {
+                contador += this.getHijoDerecho().contarHojas();
+            }
+
+            return contador;
+        }
+
+        public void recorridoEntreNiveles(int n, int m)
+        {
+
+            if (n > m || n < 0)
+            {
+                Console.WriteLine("Rango de niveles inválido.");
+                return;
+            }
+
+            Cola<(ArbolBinario<T>, int)> cola = new Cola<(ArbolBinario<T>, int)>();
+            cola.encolar((this, 0)); // Empezamos con la raíz en el nivel 0
+
+            while (cola.cuantos() > 0)
+            {
+                var (nodoActual, nivel) = cola.desencolar();
+
+                // Si estamos dentro del rango, imprimimos el dato
+                if (nivel >= n && nivel <= m)
+                {
+                    Console.WriteLine(nodoActual.getDatoRaiz());
+                }
+
+                // Si el nivel actual es mayor que 'm', salimos, ya que no quedan niveles válidos por recorrer
+                if (nivel > m)
+                {
+                    break;
+                }
+
+                // Agregar hijos al siguiente nivel
+                if (nodoActual.getHijoIzquierdo() != null)
+                {
+                    cola.encolar((nodoActual.getHijoIzquierdo(), nivel + 1));
+                }
+
+                if (nodoActual.getHijoDerecho() != null)
+                {
+                    cola.encolar((nodoActual.getHijoDerecho(), nivel + 1));
+                }
+            }
+        }
+    
+    }
+}
---
+++ Entreaga_n°1_Corregido/ejercicio1/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejercicio1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Creo el arbol y sus respectivos arbol-nodo (subArbol).
+            ArbolBinario<int> a = new ArbolBinario<int>(1);
+            ArbolBinario<int> b = new ArbolBinario<int>(2);
+            ArbolBinario<int> c = new ArbolBinario<int>(3);
+            ArbolBinario<int> d = new ArbolBinario<int>(4);
+            ArbolBinario<int> e = new ArbolBinario<int>(5);
+            ArbolBinario<int> f = new ArbolBinario<int>(6);
+            ArbolBinario<int> g = new ArbolBinario<int>(7);
+
+            a.agregarHijoIzquierdo(b); 
+                                        //creamos la relacion-union, entre los nodos. 
+            a.agregarHijoDerecho(c);
+
+
+            b.agregarHijoIzquierdo(d);
+
+            c.agregarHijoIzquierdo(e);
+            c.agregarHijoDerecho(f);
+
+            e.agregarHijoIzquierdo(g);
+
+         
+            (nuevo(a)).recorridoPorNiveles();// se hace uso del metodo nuevo(ArbolGeneral<int> arbol),
+                                             // y a su vez se utiliza el recorrido Por Niveles 
+                                             //para que imprima los valores del arbol y verificar
+                                             //el correcto funcionamiento del metodo "nuevo(a)".
+          
+              
+        }
+
+        //Se corrigio la estructura empleada para Arbol(hijoIzquierdo e hijoDerecho). Los recorridos
+        //preOrden, inOrden, postOrden: se resolvieron de manera recursiva para cumplir 
+        //los requisitos. Debido a las peticiones del metodo "nuevo" que dan una complejidad
+        //que evita una resolucion de manera recursiva directa y eficiente, insistiendo
+        //en una solucion recursiva claro, se tuvo que obtar por una resolucion Iterativa
+        //en el metodo "nuevo".
+        public static ArbolBinario<int> nuevo(ArbolBinario<int> arbol)
+        {
+            // Caso base: Si el árbol original es null, retornamos null
+            if (arbol == null)
+            {
+                return null;
+            }
+
+            // Crear la raíz del nuevo árbol
+            ArbolBinario<int> nuevoArbol = new ArbolBinario<int>(arbol.getDatoRaiz());
+
+            // Crear colas para realizar el recorrido de ambos árboles simultáneamente
+            Cola<(ArbolBinario<int>, ArbolBinario<int>)> cola = new Cola<(ArbolBinario<int>, ArbolBinario<int>)>();
+            cola.encolar((arbol, nuevoArbol));
+
+            // Bucle para procesar cada nodo en el árbol original
+            while (!cola.EstaVacia())
+            {
+                var (nodoOriginal, nodoNuevo) = cola.desencolar();
+
+                // Procesamos el hijo izquierdo
+                if (nodoOriginal.getHijoIzquierdo() != null)
+                {
+                    // Calculamos el valor del nuevo hijo izquierdo como la suma del nodo padre y el hijo izquierdo original
+                    int nuevoValorIzquierdo = nodoOriginal.getDatoRaiz() + nodoOriginal.getHijoIzquierdo().getDatoRaiz();
+                    ArbolBinario<int> nuevoHijoIzquierdo = new ArbolBinario<int>(nuevoValorIzquierdo);
+
+                    // Lo agregamos al nuevo árbol y encolamos el par para el siguiente nivel
+                    nodoNuevo.agregarHijoIzquierdo(nuevoHijoIzquierdo);
+                    cola.encolar((nodoOriginal.getHijoIzquierdo(), nuevoHijoIzquierdo));
+                }
+
+                // Procesamos el hijo derecho
+                if (nodoOriginal.getHijoDerecho() != null)
+                {
+                    // El hijo derecho se copia directamente
+                    ArbolBinario<int> nuevoHijoDerecho = new ArbolBinario<int>(nodoOriginal.getHijoDerecho().getDatoRaiz());
+                    nodoNuevo.agregarHijoDerecho(nuevoHijoDerecho);
+
+                    // Encolamos el par para el siguiente nivel
+                    cola.encolar((nodoOriginal.getHijoDerecho(), nuevoHijoDerecho));
+                }
+            }
+
+            return nuevoArbol;
+        }
+
+
+
+
+
+
+
+    }
+}
